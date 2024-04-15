d03c8869e6e8d335d4b91a9b1242f155e527a0a2
Eneson <enersonsony7@gmail.com>
2024-04-15 11:35:40 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   //baseURL: 'https://adote-app.herokuapp.com'
-  //baseURL: 'http:192.168.1.104:3000'
-  baseURL: 'https://adote-app.onrender.com'
+  baseURL: 'http:192.168.1.101:3000'
+  //baseURL: 'https://adote-app.onrender.com'
 })
 
 export default api
